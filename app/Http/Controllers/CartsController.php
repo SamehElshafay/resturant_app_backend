7533d1dc8fcae0856d6da7760297f3ad0512b086
Sameh Walied Mohamed Elshafay <73129344+SamehElshafay@users.noreply.github.com>
2026-01-06 01:13:10 +0200

    add order time

--- app/Http/Controllers/CartsController.php
+++ app/Http/Controllers/CartsController.php
@@ -315,7 +315,7 @@ class CartsController extends Controller {
                 'other_user.address'           => 'sometimes|string' ,
                 'payment_method_id' => 'required|integer|exists:method,id',
                 'address_id'        => 'sometimes|integer' ,
-                'time_order'        => 'sometimes|timestamp',
+                'time_order'        => 'sometimes|date',
                 'note'              => 'sometimes|string' ,
             ]);
             
