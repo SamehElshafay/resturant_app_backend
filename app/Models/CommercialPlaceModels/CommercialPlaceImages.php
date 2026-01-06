d86df638551b5c7d902092e1849959c9004b492e
Sameh Walied Mohamed Elshafay <73129344+SamehElshafay@users.noreply.github.com>
2026-01-06 08:41:03 +0200

    ehance image uploade

--- app/Models/CommercialPlaceModels/CommercialPlaceImages.php
+++ app/Models/CommercialPlaceModels/CommercialPlaceImages.php
@@ -27,7 +27,7 @@ class CommercialPlaceImages extends Model
     protected $appends = ['full_path'];
 
     public function getFullPathAttribute(){
-        return url($this->image_path) ;
+        return url($this->path) ;
     }
 
 }
