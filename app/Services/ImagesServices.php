3074f3beb40aa49114a128965aad403226b2fcc3
Sameh Walied Mohamed Elshafay <73129344+SamehElshafay@users.noreply.github.com>
2026-01-06 06:53:08 +0200

    ehance image uploade

--- app/Services/ImagesServices.php
+++ app/Services/ImagesServices.php
@@ -2,14 +2,13 @@
 
 namespace App\Services;
 
-use App\Models\AuditLog;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage ;
+use Illuminate\Support\Str ;
 
 class ImagesServices {
     static public function uploadImage(string $categoryName , $image) {
         $fileName = Str::random(10) . '_' . time() . '.' . $image->getClientOriginalExtension();
-        $path = Storage::url($image->storeAs($categoryName , $fileName , 'public'));
+        $path = $image->storeAs($categoryName , $fileName , 'public') ;
         return $path ;
     }
 }
