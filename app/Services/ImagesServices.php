c2cd599ccfac018deedfd74fdb091b2694f62e00
Sameh Walied Mohamed Elshafay <73129344+SamehElshafay@users.noreply.github.com>
2026-01-06 06:58:46 +0200

    ehance image uploade

--- app/Services/ImagesServices.php
+++ app/Services/ImagesServices.php
@@ -7,8 +7,19 @@ use Illuminate\Support\Str ;
 
 class ImagesServices {
     static public function uploadImage(string $categoryName , $image) {
-        $fileName = Str::random(10) . '_' . time() . '.' . $image->getClientOriginalExtension();
+        /*$fileName = Str::random(10) . '_' . time() . '.' . $image->getClientOriginalExtension();
         $path = $image->storeAs($categoryName , $fileName , 'public') ;
-        return $path ;
+        return $path ;*/
+        $fileName = Str::random(10) . '_' . time() . '.' . $image->getClientOriginalExtension();
+
+        $path = public_path("uploads/$categoryName");
+
+        if (!file_exists($path)) {
+            mkdir($path, 0755, true);
+        }
+
+        $image->move($path, $fileName);
+
+        return "/uploads/$categoryName/$fileName";
     }
 }
